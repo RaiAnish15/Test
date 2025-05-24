f3121facdce064563474275372f034476af7c1b3
RaiAnish15 <114273678+RaiAnish15@users.noreply.github.com>
2025-05-24 14:57:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os
 
 st.set_page_config(page_title="Smart Agri: Basmati Intelligence Portal", layout="wide")
-st.title("Smart Agri: Basmati Intelligence Portal")
+st.title("Agri: Basmati Intelligence Portal")
 
 # Top-level section selection including "Quality"
 section = st.radio("Select Section", options=["Meteorological Variable", "Market", "What If", "Quality"], horizontal=True)
